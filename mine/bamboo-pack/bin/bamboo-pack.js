c05d70b1c82c2fb3a1055d2f9d7b40b9e2609745
HumorYi <bamboo8493@126.com>
2019-07-18 05:37:29 +0800

    fixed mine/bamboo-pack => lib/Compiler.js(config) + bin/bamboo-pack.js(bamboo-pack run env)

--- mine/bamboo-pack/bin/bamboo-pack.js
+++ mine/bamboo-pack/bin/bamboo-pack.js
@@ -1,5 +1,7 @@
+#! /usr/bin/env node
+
 /**
- * #! /user/bin/env node
+ * #! /usr/bin/env node 声明执行环境，必须要加
  *
  * npm link 把 package.json 下面的 bin字段里面的内容映射到 node 全局安装目录
  *
@@ -9,8 +11,12 @@
  * D:\software-family\node\global\node_modules\bamboo-pack ->
  *  I:\Upan\bamboo\learning\webpack\webpack-mine\bamboo-pack
  *
+ * 由于已安装到全局，所以项目目录无需再安装引用，直接使用即可，启动方式：bamboo-pack
  *
- * npm link bamboo-pack 在需要注册包的目录中运行，注册引用包路径
+ * 如果想添加到package.json的scripts命令中，如 "build-bamboo-pack": "bamboo-pack",
+ * 因为scripts下的命令都是直接插到当前目录下的node_modules，很显然并不存在，
+ * 使用 npm link bamboo-pack 在需要注册包的目录中运行，注册引用包路径，
+ * 启动方式 npm run build-bamboo-pack *
  *
  * I:\Upan\bamboo\learning\webpack\webpack-mine\test\node_modules\bamboo-pack ->
  *  D:\software-family\node\global\node_modules\bamboo-pack ->
--- mine/bamboo-pack/lib/Compiler.js
+++ mine/bamboo-pack/lib/Compiler.js
@@ -9,12 +9,26 @@ const types = require('@babel/types');
 // @babel/generator 生成ast语法树源码
 const generator = require('@babel/generator').default;
 const ejs = require('ejs');
+// 钩子库
 const { SyncHook } = require('tapable');
 
+const defaultConfig = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
+
 class Compiler {
-  constructor(config) {
+  /**
+   * Creates an instance of Compiler.
+   * @param {Object} [config={}]
+   * @memberof Compiler
+   */
+  constructor(config = {}) {
     // webpack.config.js entry output
-    this.config = config;
+    this.config = { ...defaultConfig, ...config };
     // 工作路径
     this.root = process.cwd();
     // 保存文件入口的路径 => './src/index.js'
@@ -34,15 +48,19 @@ class Compiler {
       done: new SyncHook(),
     };
   }
+  /**
+   * 启动编译
+   *
+   * @memberof Compiler
+   */
   run() {
     // 执行入口钩子
     this.hooks.entryOption.call();
     // 执行启动钩子
     this.hooks.run.call();
     // 如果用户传递了plugins参数，执行插件钩子
-    let plugins = this.config.plugins;
-    if (Array.isArray(plugins)) {
-      plugins.forEach((plugin) => plugin.apply(this));
+    if (Array.isArray(this.config.plugins)) {
+      this.config.plugins.forEach((plugin) => plugin.apply(this));
       this.hooks.afterPlugins.call();
     }
     // 执行编译钩子
@@ -67,14 +85,14 @@ class Compiler {
    */
   buildModule(modulePath, isEntry) {
     // 拿到模块的内容
-    let source = this.getSource(modulePath);
+    let fileContent = this.getFileContent(modulePath);
     // 模块id: modulePath = modulePath - this.root; => './src/index.js'
     let moduleName = this.formatPath('./' + path.relative(this.root, modulePath));
 
     if (isEntry) { this.entryId = moduleName; }
 
     // 解析源码并进行改造，返回依赖列表 => ./src 父路径
-    let { sourceCode, dependencies } = this.parse(source, path.dirname(moduleName));
+    let { sourceCode, dependencies } = this.parse(fileContent, path.dirname(moduleName));
 
     // 把相对路径和模块中的内容进行关联
     this.modules[moduleName] = sourceCode;
@@ -90,15 +108,17 @@ class Compiler {
    * @memberof Compiler
    */
   emitFile() {
+    const templateName = 'template.ejs';
     // 获取输出资源路径文件名
     let outputPathFilename = path.join(this.config.output.path, this.config.output.filename);
     // 获取模板
-    let templateStr = this.getSource(path.join(__dirname, 'template.ejs'));
+    let templateStr = this.getFileContent(path.join(__dirname, templateName));
     // 渲染模板，替换数据
     let code = ejs.render(templateStr, {
       entryId: this.entryId,
       modules: this.modules
     });
+
     // 资源中输出路径对应的代码
     this.assets[outputPathFilename] = code;
     // 把代码写到输出路径中
@@ -111,41 +131,48 @@ class Compiler {
    * @returns {String}
    * @memberof Compiler
    */
-  getSource(modulePath) {
-    let content = fs.readFileSync(modulePath, 'utf8');
+  getFileContent(modulePath) {
+    let fileContent = fs.readFileSync(modulePath, 'utf-8');
 
-    // 遍历规则
-    this.config.module.rules.forEach((rule) => {
-      // 获取规则中的正则和需要的loader
-      let { test, use } = rule;
-      let useMaxIndex = use.length - 1;
+    // 如果携带了模块解析规则，则遍历规则
+    if (this.config.module
+      && this.config.module.rules
+      && Array.isArray(this.config.module.rules)
+    ) {
+      this.config.module.rules.forEach((rule) => {
+        // 获取规则中的正则和需要的loader
+        let { test, use } = rule;
+        let useMaxIndex = use.length - 1;
 
-      // 正则验证成功，使用loader解析
-      if (test.test(modulePath)) {
-        // 获取loader
-        while (useMaxIndex >= 0) {
-          // loader是从右至左，即从后往前解析
-          content = require(use[useMaxIndex--])(content);
+        // 正则验证成功，使用loader解析
+        if (test.test(modulePath)) {
+          // 获取loader
+          while (useMaxIndex >= 0) {
+            // loader是从右至左，即从后往前解析
+            fileContent = require(use[useMaxIndex--])(fileContent);
+          }
         }
-      }
-    });
+      });
+    }
 
-    return content;
+    return fileContent;
   }
   /**
-   * 解析源码，使用ast解析语法树，进行源码转移
+   * 解析源码(文件内容)，使用ast解析语法树，进行源码转移
    *
-   * @param {String} source
+   * @param {String} fileContent
    * @param {String} parentPath
    * @returns { sourceCode, dependencies }
    * @memberof Compiler
    */
-  parse(source, parentPath) {
+  parse(fileContent, parentPath) {
+    const nodeCallName = 'require';
+    const nodeCallReplaceName = '__webpack_require__';
+    let self = this;
     // 通过babylon解析源码得到ast语法树
-    let ast = babylon.parse(source);
+    let ast = babylon.parse(fileContent);
     // 模块依赖的数组
     let dependencies = [];
-    let self = this;
 
     // 通过traverse遍历ast语法树的节点
     traverse(ast, {
@@ -154,9 +181,9 @@ class Compiler {
         // 获取模块路径对应的节点
         let node = modulePath.node;
 
-        if (node.callee.name === 'require') {
+        if (node.callee.name === nodeCallName) {
           // 修改节点的调用名
-          node.callee.name = '__webpack_require__';
+          node.callee.name = nodeCallReplaceName;
           // 获取模块的引用名
           let moduleName = node.arguments[0].value;
           // 给模块的引用名加后缀 => ./a.js
@@ -174,16 +201,13 @@ class Compiler {
     // 生成ast语法树源码
     let sourceCode = generator(ast).code;
 
-    return {
-      sourceCode,
-      dependencies
-    }
+    return { sourceCode, dependencies }
   }
   /**
    * 格式化路径 \ => /
    *
    * @param {String} path
-   * @returns
+   * @returns {String}
    * @memberof Compiler
    */
   formatPath(path) {
